d1b8a766677cb39306332004eb8d917de5805d9b
Twin Panichsombat <roofimon@gmail.com>
2017-07-24 17:08:05 +0900

    Move classes

--- src/test/com/bank/service/internal/DefaultTransferWindowTest.java
+++ src/test/com/bank/service/internal/DefaultTransferWindowTest.java
@@ -1,10 +1,9 @@
 package com.bank.service.internal;
 
+import com.bank.domain.CurrentTime;
 import com.bank.domain.DefaultTransferWindow;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.time.LocalTime;
 
 public class DefaultTransferWindowTest {
     //private final
@@ -20,19 +19,6 @@ public class DefaultTransferWindowTest {
         CurrentTime currentTime = new CurrentTime("22:05:59");
         DefaultTransferWindow transferWindow = new DefaultTransferWindow("10:00:00", "22:00:00");
         Assert.assertFalse(transferWindow.isValidTimeForTransferMoney(currentTime.getCurrentTime()));
-    }
-
-    class CurrentTime{
-        private String specificTime = null;
-        public CurrentTime(String initial){
-            this.specificTime = initial;
-        }
-
-        public LocalTime getCurrentTime() {
-            if(this.specificTime != null)
-                return LocalTime.parse(this.specificTime);
-            return LocalTime.now();
-        }
     }
 
 }
