c2ba0b4fcd5cf873f23a821c25cf700ef8cea6e6
Twin Panichsombat <roofimon@gmail.com>
2017-07-25 15:21:08 +0900

    Create default constructor

--- src/main/com/bank/domain/CurrentTime.java
+++ src/main/com/bank/domain/CurrentTime.java
@@ -1,11 +1,18 @@
 package com.bank.domain;
 
+import org.omg.CORBA.Current;
+
 import java.time.LocalTime;
 
 public class CurrentTime {
+
     private String specificTime = null;
     public CurrentTime(String initial){
         this.specificTime = initial;
+    }
+
+    public CurrentTime() {
+
     }
 
     public LocalTime getCurrentTime() {
